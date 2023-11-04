ac42ef3d73b004fa3ce8a50d09e7406325daa50b
hetvi <hvkavathiya123@gmail.com>
2023-11-04 12:36:49 +0530

    change password

---
+++ application/controllers/Change_password.php
@@ -0,0 +1,65 @@
+<?php
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Change_password extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        access_level_admin();
+    }
+
+    public function index()
+    {
+        $page_data['page_title'] = 'Change Password';
+        $page_data['page_name'] = 'change_password';
+        $page_data['user_type'] = $this->session->userdata('user_type');
+        $user_type = $this->session->userdata('user_type');
+        return $this->load->view('admin/common', $page_data);
+    }
+
+    public function password()
+    {
+        $this->form_validation->set_rules('old_password', 'Old Password', 'required');
+        $this->form_validation->set_rules('new_password', 'New Password', 'required');
+        $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[new_password]');
+
+        if ($this->form_validation->run() == false) {
+            $message['success'] = 0;
+            $message['message'] = validation_errors();
+            $this->session->set_flashdata('flash', $message);
+            redirect(base_url('change_password'), 'refresh');
+        } else {
+
+            $user_id = $this->session->userdata('login')['user_id'];
+            $user_type = $this->session->userdata('user_type');
+            $data = $this->input->post();
+
+            $old_password = $this->db->get_where('user', array('id' => $user_id))->row('password');
+            // echo   $old_password, sha1($data['old_password']);
+            // exit;
+            if (sha1($data['old_password']) == $old_password) {
+                $update = $this->db->where('id', $user_id)->update('user', array('password' => sha1($data['new_password'])));
+                if ($update) {
+                    $message['success'] = 1;
+                    $message['message'] = "User password change  SuccessFully.";
+                    $this->session->set_flashdata('flash', $message);
+                    redirect(base_url('change_password'));
+                } else {
+                    $message['success'] = 0;
+                    $message['message'] = "Failed to changed password.";
+                    $this->session->set_flashdata('flash', $message);
+                    redirect(base_url('change_password'));
+                }
+            } else {
+                $message['success'] = 0;
+                $message['message'] = "Incorrect Current password.";
+                $this->session->set_flashdata('flash', $message);
+                redirect(base_url('change_password'));
+            }
+        }
+    }
+}
---
+++ application/views/change_password.php
@@ -0,0 +1,77 @@
+<div class="container-xxl flex-grow-1 container-p-y pt-0">
+    <h4 class="py-2 mb-2"><span class="text-muted fw-light"><?= $user_type ?> / </span> Change Password </h4>
+    <!-- Change Password -->
+    <div class="card mb-4">
+        <h5 class="card-header">Change Password</h5>
+        <div class="card-body">
+            <form class="browser-default-validation mb-3 needs-validation" id="formAccountSettings" method="POST" novalidate id="userForm" action="<?= base_url('change_password/password'); ?>">
+                <div class="row">
+                    <div class="mb-3 col-md-6 form-password-toggle">
+                        <div class="form-floating form-floating-outline">
+                            <input class="form-control required" type="password" name="old_password" id="old_password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" required />
+                            <label for="currentPassword">Current Password</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row g-3 mb-4">
+                    <div class="col-md-6 form-password-toggle">
+                        <div class="form-floating form-floating-outline">
+                            <input class="form-control required" type="password" id="new_password" name="new_password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" required />
+                            <label for="newPassword">New Password</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row g-3 mb-4">
+                    <div class="col-md-6 form-password-toggle">
+                        <div class="form-floating form-floating-outline">
+                            <input class="form-control required" type="password" name="confirm_password" id="confirm_password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" required />
+                            <label for="confirmPassword">Confirm New Password</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row my-4">
+                    <div class="col-md-3">
+                        <label class="form-check">
+                            <input class="form-check-input pass-show" type="checkbox">
+                            <span class="form-check-label">Show password</span>
+                        </label>
+                    </div>
+                </div>
+                </ul>
+                <div class="mt-4">
+                    <button type="submit" class="btn btn-primary me-2">Save changes</button>
+                    <button type="reset" class="btn btn-outline-secondary">Cancel</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+    <!--/ Change Password -->
+
+    <script>
+        $(document).ready(function() {
+            $('.pass-show').change(function() {
+                var op = $('#old_password');
+                var np = $('#new_password');
+                var cp = $('#confirm_password');
+                (op.attr('type') == 'password') ? op.attr('type', 'text'): op.attr('type', 'password');
+                (np.attr('type') == 'password') ? np.attr('type', 'text'): np.attr('type', 'password');
+                (cp.attr('type') == 'password') ? cp.attr('type', 'text'): cp.attr('type', 'password');
+            })
+        })
+    </script>
+    <?php if ($alert = $this->session->flashdata('flash')) { ?>
+        <script>
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: false,
+            })
+            Toast.fire({
+                icon: '<?= $alert['class'] ?>',
+                title: '<?= $alert['message'] ?>'
+            })
+        </script>
+    <?php } ?>
